69a4957947aad9513977324bd3d80e10cf738e8f
Suzanne Tordjman <suzannetor@gmail.com>
2022-04-12 20:23:59 -0400

    add controllers restaurant and review

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,4 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_blank: false }
-
-
 end
