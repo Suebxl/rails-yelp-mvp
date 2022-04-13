6b072e1c7870709fd1a77995f52e9ba320344584
Suzanne Tordjman <suzannetor@gmail.com>
2022-04-12 21:36:55 -0400

    restaurant controller done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "cleaning the database ..."
+Restaurant.destroy_all
+puts " Creating restaurants ..."
+
+dragon = Restaurant.create(
+  name: "The Chinese Dragon",
+  address: "125 Bank street",
+  phone_number: "613-265-1245",
+  category: "chinese"
+)
+
+pizza = Restaurant.create(
+  name: "La pizza italiana",
+  address: "129 Bank street",
+  phone_number: "613-265-1234",
+  category: "italian"
+)
+
+frite = Restaurant.create(
+  name: "La frite",
+  address: "133 Bank street",
+  phone_number: "999-888-1234",
+  category: "belgian"
+)
+
+lucien = Restaurant.create(
+  name: "Chez lucien",
+  address: "137 Bank street",
+  category: "french"
+)
+
+sushi = Restaurant.create(
+  name: "Sushi",
+  address: "128 Bank street",
+  category: "japanese"
+)
